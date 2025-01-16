0b46492afeefc600f4563e9ea70a4b0e85781aa7
Remco <remcostoeten@hotmail.com>
2025-01-16 10:25:55 +0100

    feat: work on configuration  and rename old setup

---
+++ shell-config/injectors/zsh_injector.sh
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------
+# Default configuration settings for .zshrc 
+# -----------------------------------------------------------------------------
+
+# Allow navigation without typinig cd
+setopt AUTO_CD
+
+# Enable autocorrect
+setopt CORRECT
+
+# Share history across all sessions
+setopt SHARE_HISTORY
+
+# History file settings 
+HISTFILE= 
